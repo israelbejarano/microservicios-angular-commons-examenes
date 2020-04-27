00bb8e3487a9c18cfb9b0e0d256b0411e2a2aefd
israel <israel.bejarano.a@gmail.com>
2020-04-27 20:20:13 +0200

    commit inicial

---
+++ microservicios-commons-examenes/src/main/java/com/curso/microservicios/commons/examenes/MicroserviciosCommonsExamenesApplication.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.commons.examenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosCommonsExamenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosCommonsExamenesApplication.class, args);
+	}
+
+}
