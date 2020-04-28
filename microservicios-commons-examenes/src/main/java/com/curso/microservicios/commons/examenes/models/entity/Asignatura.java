64f36ee5db34cfbe5c2b1ccb927f2eaabe18d60f
israel <israel.bejarano.a@gmail.com>
2020-04-28 11:28:49 +0200

    entidad asignatura base

---
+++ microservicios-commons-examenes/src/main/java/com/curso/microservicios/commons/examenes/models/entity/Asignatura.java
@@ -0,0 +1,39 @@
+package com.curso.microservicios.commons.examenes.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "asignaturas")
+public class Asignatura implements Serializable {
+
+	private static final long serialVersionUID = 5770833074692583806L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nombre;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
